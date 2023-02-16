95854291d88158e9ce70f11de36de1a50ecd8f6e
Charlene <charlene.dhon@farsight-vr.com>
2023-02-16 16:32:27 +0100

    Wrap Text OK Img OK

--- src/views/AboutDisconnectedView.vue
+++ src/views/AboutDisconnectedView.vue
@@ -1,34 +1,37 @@
 <template>
     <div class="fullPage">
-        <div class="explanationLeft">
-            <img src="@/assets/Plante/Plante_8.webp" ALIGN=”top” alt="" class="imgPlantExplanation">
+        <div class="explanationContent">
+            <img src="@/assets/Plante/Plante_8.webp" alt="" class="imgPlantExplanation">
 
-            <div class="textExplanation">
-                <div class="Title">
-                    <h1> A'rosa'ge, c'est quoi ?</h1>
-                </div>
-                <div class="explanation">
-                    <p>L’entreprise “A’rosa-je” aide les particuliers à prendre soin de leurs plantes.
-                        Fondée en 1984 elle a tout d’abord été composée d’une petite équipe de botanistes dans une seule
-                        ville et est maintenant composée de plus de 1500 botanistes répartis sur toute la France qui
-                        rendent service aux propriétaires de plantes de deux façons :</p>
+            <h1>A’Rosa-je, c’est quoi ?</h1>
+            <p><br>L’entreprise “A’rosa-je” aide les particuliers à prendre soin de leurs plantes.
+                Fondée en 1984 elle a tout d’abord été composée d’une petite équipe de botanistes dans une
+                seule
+                ville et est maintenant composée de plus de 1500 botanistes répartis sur toute la France qui
+                rendent service aux propriétaires de plantes de deux façons :
 
-                    <p>• En allant garder leurs plantes lorsque les propriétaires sont absents</p>
-                    <p>• En prodiguant des conseils d’entretien afin que les propriétaires s’occupent de mieux en mieux
-                        de leurs plantes.</p>
-                </div>
-                <!-- <img src="@/assets/Plante/Plante_9.jpg" alt="" class="imgPlantExplanation_2"> -->
-            </div>
+                <br>• En allant garder leurs plantes lorsque les propriétaires sont absents
+                <br>• En prodiguant des conseils d’entretien afin que les propriétaires s’occupent de mieux en
+                mieux
+                de leurs plantes.
+
+            </p>
         </div>
-        <ul class="stepList">
-            <textImg :number="1" :textStep="textStep1"> </textImg>
-            <textImg :number="2" :textStep="textStep2"> </textImg>
-            <textImg :number="3" :textStep="textStep3"> </textImg>
-            <textImg :number="4" :textStep="textStep4"> </textImg>
-            <textImg :number="5" :textStep="textStep5"> </textImg>
 
-        </ul>
-        <img src="@/assets/Plante/Plante_3.jpeg" alt="" class="imgPlantExplanation_2">
+
+        <!-- <img src="@/assets/Plante/Plante_9.jpg" alt="" class="imgPlantExplanation_2"> -->
+
+        <div class="steps">
+            <ul class="stepList">
+                <textImg :number="1" :textStep="textStep1"> </textImg>
+                <textImg :number="2" :textStep="textStep2"> </textImg>
+                <textImg :number="3" :textStep="textStep3"> </textImg>
+                <textImg :number="4" :textStep="textStep4"> </textImg>
+                <textImg :number="5" :textStep="textStep5"> </textImg>
+
+            </ul>
+            <img src="@/assets/Plante/Plante_3.jpeg" alt="" class="imgPlantExplanation_2">
+        </div>
     </div>
 </template>
 <script>
@@ -66,31 +69,30 @@ export default {
     flex-direction: column;
 }
 
-.explanationLeft {
-    display: flex;
+.explanationContent {
+    display: block;
 }
-
-
 .imgPlantExplanation {
-    width: 150px;
-    height: 150px;
+    display: block;
+    width: 200px;
+    height: 200px;
     padding: 10px;
-    padding-top: 130px;
+    margin: 10px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    float: left;
+
+    margin: 0 0 0 15px;
+
 }
 
 .imgPlantExplanation_2 {
     width: 400px;
     align-self: center;
-    margin: 20px;
-    padding: auto;
-}
-
-.Title {
-    text-decoration: underline;
-}
-
-.explanation {
-    font-size: 14px;
+    margin: 10%;
+    margin-top: 5%;
 }
 
 .stepList {
@@ -106,4 +108,21 @@ export default {
     flex-direction: column;
     align-items: flex-start;
 }
+
+.steps{
+    display: flex;
+    flex-direction: row;
+}
+
+@media (max-width : 1300px) {
+    .imgPlantExplanation {
+        float: none;
+    }
+    .steps{
+        flex-direction: column;
+    }
+
+    
+}
+
 </style>
