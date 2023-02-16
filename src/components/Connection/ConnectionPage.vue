d76f0ca58de2c41801fc44c309eef9dd9b6be1f8
timotheegarot <timotheegarot@hotmail.com>
2023-02-16 14:36:10 +0100

    Add routes when buttons clicked

--- src/components/Connection/ConnectionPage.vue
+++ src/components/Connection/ConnectionPage.vue
@@ -1,108 +1,119 @@
 <template>
-    <div class="container">
-      <div class="container-connection">
-        <h1>CONNEXION</h1>
+  <div class="container">
+    <div class="container-connection">
+      <h1>CONNEXION</h1>
 
-        <form id="formConnection">
-          <div class="form-field">
-            <label for="mailAddress">Adresse mail : </label>
-            <input type="text" id="mailAddress" placeholder="Entrez votre adresse mail">
-          </div>
-          
-          <div class="form-field">
-            <label for="password">Mot de passe : </label>
-            <input type="password" id="password" placeholder="Entrez votre mot de passe">
-          </div>
+      <form id="formConnection">
+        <div class="form-field">
+          <label for="mailAddress">Adresse mail : </label>
+          <input type="text" id="mailAddress" placeholder="Entrez votre adresse mail">
+        </div>
+        
+        <div class="form-field">
+          <label for="password">Mot de passe : </label>
+          <input type="password" id="password" placeholder="Entrez votre mot de passe">
+        </div>
 
-          <a href="mailto:admin@arosaje.fr" id="forgotPassword">Mot de passe oublié</a>
-          
-          <div>
-            <button class="btn-Connection" id="submit">Connexion</button>
-          </div>
-          
-          <div>
-            <a class="notRegistered" href="mailto:admin@arosaje.fr" id="notYetRegistered">Pas encore membre ? Inscris-toi !</a>
-          </div>
-        </form>
-      </div>
+        <a href="mailto:admin@arosaje.fr" id="forgotPassword">Mot de passe oublié</a>
+        
+        <div>
+          <button class="btn-Connection" id="submit" @click="goToView(routeLogin)">Connexion</button>
+        </div>
+        
+        <div>
+          <a class="notRegistered" href="" id="notYetRegistered" @click="goToView(routeRegister)">Pas encore membre ? Inscris-toi !</a>
+        </div>
+      </form>
     </div>
-  </template>
-  <script>
-    export default {
-      name: 'ConnectionView'
-    }
-  
-  </script>
-  <style>
-    h1 {  /* CSS Global */
-      text-align: center;
-      margin-bottom: 10px;
-    }
+  </div>
+</template>
+<script>
+  export default {
+    name: 'ConnectionView',
+    data() {
+      return {
+        routeRegister: "register",
+        routeLogin: "about",
+      }
+    },
 
-    label {
-      margin-bottom: 2px;
-      font-size: large;
-      font-weight: bold;
+    methods: {
+      goToView(path){
+        this.$router.push({name:path})
+      }
     }
+  }
 
-    input {
-      padding: 10px;
-    }
+</script>
+<style>
+  h1 {  /* CSS Global */
+    text-align: center;
+    margin-bottom: 10px;
+  }
 
-    .container {
-      height: auto;
-      margin: 0 auto;
-    }
-  
-    .container-connection {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      background-color: var(--main-bg-color);
-      border-radius: 10px;
-      border: 2px solid var(--main-container-border);
-      margin: 50% 0 0 0;
-    }
+  label {
+    margin-bottom: 2px;
+    font-size: large;
+    font-weight: bold;
+  }
 
-    .form-field {
-      display: flex;
-      flex-direction: column;
-    }
+  input {
+    padding: 10px;
+  }
 
-    .notRegistered {
-      margin-bottom: 10px;
-      font-size: large;
-    }
+  .container {
+    height: auto;
+    margin: 0 auto;
+  }
 
-    .btn-Connection { /* CSS Global -> btn-validate */
-      margin-bottom: 25px;
-      width: 200px;
-      border-radius: 20px;
-      color: #fff;
-      border: 1px solid #bad9cb;
-      background-color: var(--btn-validate-color);
-      font-size: 30px;
-    }
+  .container-connection {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--main-bg-color);
+    border-radius: 10px;
+    border: 2px solid var(--main-container-border);
+  }
 
-    #mailAddress, #password{
-      margin-bottom: 10px;
-      border-radius: 10px;
-      border: 1px solid #519069;
-      width: 300px;
-      font-size: large;
-    }
+  .form-field {
+    display: flex;
+    flex-direction: column;
+  }
 
-    #formConnection {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin: 10px;
-    }
-  
-    #forgotPassword {
-      display: flex;
-      justify-content: center;
-      margin-bottom: 25px;
-      font-size: large;
-    }
-  </style>
+  .notRegistered {
+    margin-bottom: 10px;
+    font-size: large;
+  }
+
+  .btn-Connection { /* CSS Global -> btn-validate */
+    margin-bottom: 25px;
+    width: 200px;
+    border-radius: 20px;
+    color: #fff;
+    border: 1px solid #bad9cb;
+    background-color: var(--btn-validate-color);
+    font-size: 30px;
+  }
+
+  #mailAddress, #password{
+    margin-bottom: 10px;
+    border-radius: 10px;
+    border: 1px solid #519069;
+    width: 300px;
+    font-size: large;
+  }
+
+  #formConnection {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+  }
+
+  #forgotPassword {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 25px;
+    font-size: large;
+  }
+</style>
