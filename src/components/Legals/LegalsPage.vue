e08fac2098968a07cee86505393e7bb80333b264
timotheegarot <timotheegarot@hotmail.com>
2023-02-17 16:11:18 +0100

    Text fit in container

--- src/components/Legals/LegalsPage.vue
+++ src/components/Legals/LegalsPage.vue
@@ -4,10 +4,10 @@
       <h1>Mentions légales</h1>
     </div>
 
-    <div>
+    <div class="legals-text">
       <p><strong>1 - Édition du site</strong></p>
       
-      <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://www.arosage.fr/ l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:</p>
+      <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://www.arosaje.fr/ l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:</p>
       <p>Propriétaire du site : Les 4 bota’clic - Contact : arosaje@epsi.fr Les 4 bota'clic - Adresse : 10 rue fernand robert, 35000 Rennes.</p>
       <p>Identification de l'entreprise : Les 4 bota’clic au capital social de  0€ - SIREN : - RCS ou RM : - Adresse postale : 10 rue fernand robert, 35000 Rennes</p>
     
@@ -15,45 +15,35 @@
       <p>Hébergeur : OVH SAS - 2 rue Kellermann - BP 80157 - 59053 Roubaix Cedex 1 - Téléphone : 0615487529</p>
       <p>Délégué à la protection des données : Thomas Le Corvec - ThomasLeCorvec@Epsi.fr</p>
       <p>Autres contributeurs : Timothée Garot, Thomas Le Corvec, Charlène D'hon, Thomas Girardet</p>
-    </div>
-    
-    <div>
+
       <p><strong>2 - Propriété intellectuelle et contrefaçons.</strong></p>
       
       <p>Les 4 bota’clic est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.</p>
       <p>Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable des 4 Bota’clic.</p>
       <p>Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
-    </div>
 
-    <div>
       <p><strong>3 - Limitations de responsabilité.</strong></p>
       
-      <p>Les 4 bota’clic ne pourront être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://www.arosage.fr/.</p>
+      <p>Les 4 bota’clic ne pourront être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://www.arosaje.fr/.</p>
       <p>Les 4 bota’clic déclinent toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur https://www.arrosage.fr/.
-      Les 4 bota’clic s’engagent à sécuriser au mieux le site https://www.arosage.fr/, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.</p>
+      Les 4 bota’clic s’engagent à sécuriser au mieux le site https://www.arosaje.fr/, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.</p>
       <p>Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. Les 4 bota’clic se réservent le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données.</p>
       <p>Le cas échéant, Les 4 bota’clic se réservent également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).</p>
-    </div>
-    
-    <div>
+      
       <p><strong>4 - CNIL et gestion des données personnelles.</strong></p>
       
-      <p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://www.arosage.fr/ dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : Thomas Le Corvec - ThomasLeCorvec@Epsi.fr.</p>
-      <p>Pour plus d'informations sur la façon dont nous traitons vos données (type de données, finalité, destinataire...), lisez notre https://www.arosage.fr/mentions-legales/. https://www.arosage.fr/PolitiqueDeConfidentialité/</p>
-    </div>
-
-    <div>
+      <p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://www.arosaje.fr/ dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : Thomas Le Corvec - ThomasLeCorvec@Epsi.fr.</p>
+      <p>Pour plus d'informations sur la façon dont nous traitons vos données (type de données, finalité, destinataire...), lisez notre https://www.arosaje.fr/mentions-legales/. https://www.arosaje.fr/PolitiqueDeConfidentialité/</p>
+      
       <p><strong>5 - Liens hypertextes et cookies</strong></p>
 
-      <p>Le site https://www.arosage.fr/ contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers https://www.arosage.fr/.</p>
-      <p>La navigation sur le site https://www.arosage.fr/ est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.
+      <p>Le site https://www.arosaje.fr/ contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers https://www.arosaje.fr/.</p>
+      <p>La navigation sur le site https://www.arosaje.fr/ est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.
       Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.</p>
       <p>Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement.</p>
       <p>Les cookies sont enregistrés pour une durée maximale de 13 mois.</p>
-      <p>Pour plus d'informations sur la façon dont nous faisons usage des cookies, lisez notre https://www.arosage.fr/mentions-legales/.  https://www.arosage.fr/PolitiqueDeConfidentialité/</p>
-    </div>
-    
-    <div>
+      <p>Pour plus d'informations sur la façon dont nous faisons usage des cookies, lisez notre https://www.arosaje.fr/mentions-legales/.  https://www.arosaje.fr/PolitiqueDeConfidentialité/</p>
+      
       <p><strong>6 - Droit applicable et attribution de juridiction.</strong></p>
       
       <p>Tout litige en relation avec l’utilisation du site https://www.epsi.fr/ est soumis au droit français. En dehors des cas où la loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Rennes.</p>
@@ -69,5 +59,10 @@
 </script>
 
 <style>
+
+  .legals-text {
+    word-wrap: break-word;
+    width: -webkit-fill-available;
+  }
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,63 @@
 import { createRouter, createWebHistory } from "vue-router";
+// import { isConnected } from "../../api.config";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: () => import("@/views/AboutDisconnectedView.vue")
+    component: () => import("@/views/AboutDisconnectedView.vue"),
+    meta: {
+      requiredAuth: false
+    }
   },
+	{
+		path: "/about",
+		name: "about",
+		component: () => import("@/views/AboutConnectedView.vue"),
+		meta: {
+			requiredAuth: true
+		}
+	},
   {
     path: "/profil",
     name: "profil",
-    component: () => import("@/views/ProfilView.vue")
+    component: () => import("@/views/ProfilView.vue"),
+    meta: {
+      requiredAuth: true
+    }
   },
   {
     path: "/plants",
     name: "plants",
-    component: () => import("@/views/PlantsView.vue")
+    component: () => import("@/views/PlantsView.vue"),
+    meta: {
+      requiredAuth: true
+    }
   },
   {
     path: "/botanist",
     name: "botanist",
-    component: () => import("@/views/BotanistView.vue")
+    component: () => import("@/views/BotanistView.vue"),
+    meta: {
+      requiredAuth: true
+    }
   },
   {
     path: "/keeper",
     name: "keeper",
-    component: () => import("@/views/KeeperView.vue")
+    component: () => import("@/views/KeeperView.vue"),
+    meta: {
+      requiredAuth: true
+    }
   },
   {
     path: "/connection",
     name: "connection",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/ConnectionView.vue"),
+    meta: {
+      requiredAuth: false
+    }
   },
   {
     path: "/register",
@@ -40,24 +67,44 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/RegisterView.vue"),
+    meta: {
+      requiredAuth: false
+    }
   },
   {
     path: "/rgpd",
     name: "rgpd",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/RGPDView.vue"),
+    meta: {
+      requiredAuth: false
+    }
   },
   {
     path: "/legals",
     name: "legals",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/LegalsView.vue"),
-  }
+    meta: {
+      requiredAuth: true
+    }
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+// router.beforeEach((to, from, next) => {
+//   if(to.matched.some((route) => route.meta.requiredAuth) && !isConnected()){
+//     next({
+//        path: '/connection'
+//     })
+//   }
+//   else{
+//     next()
+//   }
+// })
 
 export default router;
