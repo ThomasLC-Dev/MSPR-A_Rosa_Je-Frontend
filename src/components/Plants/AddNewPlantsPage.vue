a03ae47a7311186c4e9498a5b17b0be8d327a4ca
timotheegarot <timotheegarot@hotmail.com>
2023-02-21 18:52:24 +0100

    Labels and options added

--- src/components/Plants/AddNewPlantsPage.vue
+++ src/components/Plants/AddNewPlantsPage.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="main-container">
-    <h1>add new plant</h1>
+    <h1>Ajouter une plante</h1>
+
+    <div class="form-field">
+      <label for="plantName">Nom de plante : </label>
+      <input type="text" placeholder="Nom vernaculaire ou latin">
+    </div>
+
+    <div class="form-field">
+      <label for="plantLight">Ensoleillement : </label>
+      <select id="lightSelect">
+        <option value="">Choix</option>
+        <option value="sun">Soleil</option>
+        <option value="midShadow">Mi-Ombre</option>
+        <option value="fullShadow">Ombre</option>
+      </select>
+    </div>
+    
+    <div class="form-field">
+      <label for="plantMinTemp">T°C minimale : </label>
+      <input type="text" placeholder="Valeur">
+    </div>
+
+    <div class="form-field">
+      <label for="plantMaxTemp">T°C maximale : </label>
+      <input type="text" placeholder="Valeur">
+    </div>
+
+    <div class="form-field">
+      <label for="plantWaterQuantity">Quantité d'eau : </label>
+      <input type="text" placeholder="Volume d'eau à verser">
+    </div>
+
+    <div class="form-field">
+      <label for="plantWateringFrequency">Fréquence arrosage : </label>
+      <select id="wateringSelect">
+        <option value="">Nombre</option>
+      </select>
+    </div>
+
+    <div class="form-field">
+      <label for="plantWateringType">Type arrosage : </label>
+      <input type="text" placeholder="Contenant à utiliser">
+    </div>
     
   </div>
 </template>
