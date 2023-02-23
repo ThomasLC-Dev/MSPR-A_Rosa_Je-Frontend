348bc28da40d837bb106c19602b3a2fabaafd86e
timotheegarot <timotheegarot@hotmail.com>
2023-02-23 11:21:52 +0100

    v-model added

--- src/components/Plants/AddNewPlantsPage.vue
+++ src/components/Plants/AddNewPlantsPage.vue
@@ -5,12 +5,12 @@
     <form id="formAddPlant">
       <div class="form-field">
         <label for="plantName">Nom de plante : </label>
-        <input type="text" placeholder="Nom vernaculaire ou latin" />
+        <input v-model="latinOrVerna" type="text" placeholder="Nom vernaculaire ou latin" />
       </div>
 
       <div class="form-field">
         <label for="plantLight">Ensoleillement : </label>
-        <select id="lightSelect">
+        <select v-model="sunLight" id="lightSelect">
           <option value="">Choix</option>
           <option value="sun">Soleil</option>
           <option value="midShadow">Mi-Ombre</option>
@@ -20,29 +20,29 @@
 
       <div class="form-field">
         <label for="plantMinTemp">T°C minimale : </label>
-        <input type="number" id="minTemp" placeholder="Valeur" />
+        <input v-model="lowerTemp" type="number" id="minTemp" placeholder="Valeur" />
       </div>
 
       <div class="form-field">
         <label for="plantMaxTemp">T°C maximale : </label>
-        <input type="number" id="maxTemp" placeholder="Valeur" />
+        <input v-model="higherTemp" type="number" id="maxTemp" placeholder="Valeur" />
       </div>
 
       <div class="form-field">
         <label for="plantWaterQuantity">Quantité d'eau : </label>
-        <input type="number" placeholder="Volume d'eau à verser" />
+        <input v-model="wateringQuantity" type="number" placeholder="Volume d'eau à verser" />
       </div>
 
       <div class="form-field">
         <label for="plantWateringFrequency">Fréquence arrosage : </label>
-        <select id="wateringSelect">
+        <select v-model="wateringFrequency" id="wateringSelect">
           <option value="">Nombre</option>
         </select>
       </div>
 
       <div class="form-field">
         <label for="plantWateringType">Type arrosage : </label>
-        <input type="text" placeholder="Contenant à utiliser" />
+        <input v-model="wateringContainer" type="text" placeholder="Contenant à utiliser" />
       </div>
 
       <div class="form-field">
@@ -50,22 +50,22 @@
         <img src="../../assets/Logo/add-button.png" alt="ajouter une plante"/>
         <br />
         <div class="addPhotos">
-          <input type="file" />
+          <input type="file"/>
           <div class="delete-button">
             <i class="material-icons">delete</i>
           </div>
 
-          <input type="file" />
+          <input type="file"/>
           <div class="delete-button">
             <i class="material-icons">delete</i>
           </div>
 
-          <input type="file" />
+          <input type="file"/>
           <div class="delete-button">
             <i class="material-icons">delete</i>
           </div>
 
-          <input type="file" />
+          <input type="file"/>
           <div class="delete-button">
             <i class="material-icons">delete</i>
           </div>
@@ -74,7 +74,7 @@
 
       <div class="form-field">
         <label for="plantLight">Consignes d'entretien : </label>
-        <input type="text" id="maintenanceInstructions" />
+        <input v-model="customerAdvice" type="text" id="maintenanceInstructions" />
       </div>
 
       <!-- TODO 
