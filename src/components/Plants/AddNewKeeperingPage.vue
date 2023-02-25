b073afb852ae34205edbaa78070d58c5aef3de67
timotheegarot <timotheegarot@hotmail.com>
2023-02-25 17:22:22 +0100

    Address added

--- src/components/Plants/AddNewKeeperingPage.vue
+++ src/components/Plants/AddNewKeeperingPage.vue
@@ -4,13 +4,13 @@
 
     <form id="formAddKeepering">
       <div class="form-field">
-        <label for="beginKeeperingDate">Date de début : </label>
-        <input type="date" id="beginDate">
+        <label for="beginDate">Date de début : </label>
+        <input type="date" id="startDate">
       </div>
 
       <div class="form-field">
-        <label for="endKeeperingDate">Date de fin : </label>
-        <input type="date" id="endDate">
+        <label for="endDate">Date de fin : </label>
+        <input type="date" id="finishDate">
       </div>
 
       <div class="form-field">
@@ -20,6 +20,38 @@
 
       <div class="form-field">
         <label for="contactAddress">Adresse : </label>
+        <div class="form-field-direction-gp">
+          <div class="address-road">
+            <div class="form-field field-3">
+              <label for="roadNumber">Nº de rue :</label>
+              <input type="text" id="roadNumber" placeholder="Numéro de rue" v-model="roadNumber" />
+            </div>
+            <div class="form-field field-7">
+              <label for="roadType">Type de voie :</label>
+              <input type="text" id="roadType" placeholder="Type de voie" v-model="roadType" />
+            </div>
+          </div>
+          <div class="form-field">
+            <label for="road">Nom de rue :</label>
+            <input type="text" id="road" placeholder="Nom de la rue" v-model="road" />
+          </div>
+        </div>
+        <div class="form-field-direction-gp">
+          <div class="form-field">
+            <label for="addtionalAddress">Adresse complémentaire :</label>
+            <input type="text" id="addtionalAddress" placeholder="Complément d'adresse" v-model="addtionalAddress" />
+          </div>
+          <div class="address-zip">
+            <div class="form-field field-3">
+              <label for="postalCode">Code postal :</label>
+              <input type="text" id="postalCode" placeholder="Code postal" v-model="postalCode" />
+            </div>
+            <div class="form-field field-7">
+              <label for="city">Nom de ville :</label>
+              <input type="text" id="city" placeholder="Nom de la ville" v-model="city" />
+            </div>
+          </div>
+        </div>
       </div>
     </form>
 
