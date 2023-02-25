a578982c5fe3589bd920b26f952b7c8943e17641
timotheegarot <timotheegarot@hotmail.com>
2023-02-25 17:18:27 +0100

    Start and finish date keepering

--- src/components/Plants/AddNewKeeperingPage.vue
+++ src/components/Plants/AddNewKeeperingPage.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="main-container">
-    <h1>Page pour realiser le suivi par le gardien</h1>
+    <h1>Demande de gardiennage</h1>
+
+    <form id="formAddKeepering">
+      <div class="form-field">
+        <label for="beginKeeperingDate">Date de début : </label>
+        <input type="date" id="beginDate">
+      </div>
+
+      <div class="form-field">
+        <label for="endKeeperingDate">Date de fin : </label>
+        <input type="date" id="endDate">
+      </div>
+
+      <div class="form-field">
+        <label for="contactNumber">N° de contact : </label>
+        <input type="text" id="contactPhoneNumber">
+      </div>
+
+      <div class="form-field">
+        <label for="contactAddress">Adresse : </label>
+      </div>
+    </form>
 
   </div>
 </template>
