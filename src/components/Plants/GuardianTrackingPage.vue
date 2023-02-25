7f50098ebe4da04dfe6ca2da189dc676d0187a17
timotheegarot <timotheegarot@hotmail.com>
2023-02-25 17:53:39 +0100

    Form for date and contact phone number added

--- src/components/Plants/GuardianTrackingPage.vue
+++ src/components/Plants/GuardianTrackingPage.vue
@@ -2,22 +2,21 @@
   <div class="main-container">
     <h1>Suivis</h1>
 
-    <form id="dateAndNumber">
+    <form id="formDateAndNumber">
       <div class="form-field">
         <label for="">Date de début : </label>
-        <input type="date" name="" id="">
+        <input type="date">
       </div>
 
       <div class="form-field">
         <label for="">Date de fin : </label>
-        <input type="date" name="" id="">
+        <input type="date">
       </div>
 
       <div class="form-field">
         <label for="">Numéro de contact : </label>
-        <input type="text" name="" id="">
+        <input type="text">
       </div>
-      
     </form>
     
   </div>
@@ -31,5 +30,27 @@ export default {
 
 <style scoped>
 
+label {
+  font-weight: lighter;
+  color: var(--main-title-h1);
+}
+
+.form-field {
+  flex-direction: row;
+  margin-bottom: 10px;
+}
+
+.form-field label {
+  display: inline-block;
+  width: 200px;
+  text-align: left;
+}
+
+#formDateAndNumber {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  margin: 10px;
+}
 
 </style>
