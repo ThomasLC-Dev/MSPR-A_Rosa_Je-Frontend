0b5c01cb6f929aa5410240ed03a750893dcc84fe
timotheegarot <timotheegarot@hotmail.com>
2023-02-25 17:45:07 +0100

    Dates and phone number added

--- src/components/Plants/GuardianTrackingPage.vue
+++ src/components/Plants/GuardianTrackingPage.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="main-container">
-    <h1>Demande de gardiennage</h1>
+    <h1>Suivis</h1>
 
+    <form id="dateAndNumber">
+      <div class="form-field">
+        <label for="">Date de début : </label>
+        <input type="date" name="" id="">
+      </div>
+
+      <div class="form-field">
+        <label for="">Date de fin : </label>
+        <input type="date" name="" id="">
+      </div>
+
+      <div class="form-field">
+        <label for="">Numéro de contact : </label>
+        <input type="text" name="" id="">
+      </div>
+      
+    </form>
     
   </div>
 </template>
