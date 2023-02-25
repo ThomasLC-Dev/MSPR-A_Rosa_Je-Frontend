e9ccff0eeac06df545efda09a22ca398f3d0889a
timotheegarot <timotheegarot@hotmail.com>
2023-02-25 17:40:10 +0100

    Keepering and Following pages update

--- src/components/Plants/GuardianTrackingPage.vue
+++ src/components/Plants/GuardianTrackingPage.vue
@@ -8,7 +8,7 @@
 
 <script>
 export default {
-  name: "AddNewKeeperingPage",
+  name: "GuardianTrackingPage",
 };
 </script>
 
--- src/components/Plants/RequestGuardSlotPage.vue
+++ src/components/Plants/RequestGuardSlotPage.vue
@@ -7,7 +7,7 @@
 
 <script>
 export default {
-  name: 'AddNewFollowingPage'
+  name: 'RequestGuardSlotPage'
 }
 
 </script>
