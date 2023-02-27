abfc73f4425468fb78035b28cd2c26e57cab14d6
TomGiDev <thomas.girardet@orange.fr>
2023-02-27 19:28:51 +0100

    regroupement page pour repartir de master

--- src/components/Plants/PlantCard.vue
+++ src/components/Plants/PlantCard.vue
@@ -1,82 +1,90 @@
 <template>
-  <div class="group-plant">
-    <div class="photo-part">
-      <CarousselPlant :slides="slides" :interval="3000" controls indicators />
+  <div class="card-plant">
+    <div class="row1">
+      <p type="text" class="plant-subtitle">Orchidée Phalaenopsis</p>
+      <div class="delete-button">
+        <img src="@/assets/Logo/delete-button.png" alt="Supprimer une plante" />
+      </div>
     </div>
-
-    <div class="info-part">
-      <div class="row1">
-        <p type="text" class="plant-subtitle">Orchidée Phalaenopsis</p>
-        <div class="delete-button">
-          <img src="@/assets/Logo/delete-button.png" alt="Supprimer une plante" />
-        </div>
+    <div class="group-plant">
+      <div class="photo-part">
+        <CarousselPlant :slides="slides" :interval="6000" controls indicators />
       </div>
 
-      <div class="row2">
-        <div class="row2-1">
-          <div class="logo-field">
-            <div class="logo-img-plant">
-              <img src="@/assets/Logo/Ombre.png">
-            </div>
-            <select class="logo-field-plant" id="lightSelect" v-model="sunlight" disabled>
-              <option value="fullShadow">Ombre</option>
-              <option value="sun">Soleil</option>
-              <option value="midShadow">Mi-Ombre</option>
-            </select>
+      <div class="info-part">
+        <!-- <div class="row1">
+          <p type="text" class="plant-subtitle">Orchidée Phalaenopsis</p>
+          <div class="delete-button">
+            <img src="@/assets/Logo/delete-button.png" alt="Supprimer une plante" />
           </div>
-          <div class="logo-field">
-            <div class="logo-img-plant">
-              <img src="@/assets/Logo/quantite-eau.png">
+        </div> -->
+
+        <div class="row2">
+          <div class="row2-1">
+            <div class="logo-field">
+              <div class="logo-img-plant">
+                <img src="@/assets/Logo/Ombre.png">
+              </div>
+              <select class="logo-field-plant" id="lightSelect" v-model="sunlight" disabled>
+                <option value="fullShadow">Ombre</option>
+                <option value="sun">Soleil</option>
+                <option value="midShadow">Mi-Ombre</option>
+              </select>
             </div>
-            <input type="text" class="waterQuantity logo-field-plant" disabled />
+            <div class="logo-field">
+              <div class="logo-img-plant">
+                <img src="@/assets/Logo/quantite-eau.png">
+              </div>
+              <input type="text" class="waterQuantity logo-field-plant" disabled />
+            </div>
           </div>
-        </div>
-        <div class="row2-2">
-          <div class="logo-field">
-            <div class="logo-img-plant">
-              <img src="@/assets/Logo/TemperatureBasse.png">
+          <div class="row2-2">
+            <div class="logo-field">
+              <div class="logo-img-plant">
+                <img src="@/assets/Logo/TemperatureBasse.png">
+              </div>
+              <input type="text" class="minTemp logo-field-plant" disabled />
             </div>
-            <input type="text" class="minTemp logo-field-plant" disabled />
+            <div class="logo-field">
+              <div class="logo-img-plant">
+                <img src="@/assets/Logo/TemperatureHaute.png">
+              </div>
+              <input type="text" class="maxTemp logo-field-plant" disabled />
+            </div>
           </div>
-          <div class="logo-field">
-            <div class="logo-img-plant">
-              <img src="@/assets/Logo/TemperatureHaute.png">
+
+          <div class="row2-3">
+            <div class="logo-field">
+              <div class="logo-img-plant">
+                <img src="@/assets/Logo/quantite-eau.png">
+              </div>
+              <input type="text" class="waterFrequency logo-field-plant" disabled />
             </div>
-            <input type="text" class="maxTemp logo-field-plant" disabled />
+            <div class="logo-field">
+              <div class="logo-img-plant">
+                <img src="@/assets/Logo/arrosage.png">
+              </div>
+              <input type="text" class="waterType logo-field-plant" disabled />
+            </div>
           </div>
         </div>
 
-        <div class="row2-3">
-          <div class="logo-field">
-            <div class="logo-img-plant">
-              <img src="@/assets/Logo/quantite-eau.png">
-            </div>
-            <input type="text" class="waterFrequency logo-field-plant" disabled />
+        <div class="row3">
+          <div class="form-field">
+            <p type="text" class="plant-advice-subtitle">Consignes d'entretien : </p>
+            <input type="text" class="maintenanceInstructions" v-model="customerAdvice" disabled />
           </div>
-          <div class="logo-field">
-            <div class="logo-img-plant">
-              <img src="@/assets/Logo/arrosage.png">
-            </div>
-            <input type="text" class="waterType logo-field-plant" disabled />
+          <div class="form-field">
+            <p type="text" class="plant-advice-subtitle">Conseil d'un Botaniste :</p>
+            <input type="text" class="botanistAdvice" v-model="botanistAdvice" disabled />
           </div>
         </div>
+
       </div>
-
-      <div class="row3">
-        <div class="form-field">
-          <p type="text" class="plant-advice-subtitle">Consignes d'entretien : </p>
-          <input type="text" class="maintenanceInstructions" v-model="customerAdvice" disabled />
-        </div>
-        <div class="form-field">
-          <p type="text" class="plant-advice-subtitle">Conseil d'un Botaniste :</p>
-          <input type="text" class="botanistAdvice" v-model="botanistAdvice" disabled />
-        </div>
-      </div>
-
     </div>
-  </div>
-  <div class="button-group">
-    <input class="btn-validate" type="submit" value="Modifier" />
+    <div class="button-group">
+      <input class="btn-validate" type="submit" value="Modifier" />
+    </div>
   </div>
 </template>
 
@@ -110,11 +118,24 @@ export default {
 </script>
 
 <style scoped>
+.card-plant {
+  display: fex;
+  flex-direction: column;
+}
+
 .group-plant {
   display: flex;
   flex-direction: row;
   width: 100%;
   height: 100%;
+}
+
+.plant-subtitle {
+  color: var(--main-title-h2);
+  text-align: center;
+  width: 100%;
+  font-size: xx-large;
+  font-weight: bold;
 }
 
 .photo-part {
@@ -131,14 +152,6 @@ export default {
   display: flex;
   flex-direction: row;
   margin-bottom: 30px;
-}
-
-.plant-subtitle {
-  color: var(--main-title-h2);
-  left: 0px;
-  width: 90%;
-  font-size: xx-large;
-  font-weight: bold;
 }
 
 .row1>.delete-button {
@@ -209,5 +222,56 @@ export default {
 .botanistAdvice {
   width: 450px;
   height: 300px;
+}
+
+@media (max-width: 1000px) {
+  .card-plant {
+    display: fex;
+    flex-direction: column;
+  }
+
+  .group-plant {
+    flex-direction: column;
+  }
+
+  .row1 {
+    width: 100%;
+  }
+
+  .plant-subtitle {
+    font-size: x-large;
+    margin-bottom: 0px;
+    width: 90%;
+  }
+
+  .delete-button {
+    width: 10%;
+  }
+
+  .delete-button>img {
+    position: relative;
+  }
+
+  .photo-part {
+    width: 100%;
+  }
+
+  .info-part {
+    width: 100%;
+  }
+
+  .row2,
+  .row3 {
+    flex-direction: column;
+  }
+
+  .row2-1,
+  .row2-2,
+  .row2-3 {
+    flex-direction: column;
+    margin: 1px 3px 1px 3px;
+    gap: 5px;
+  }
+
 }
 </style>
--- src/components/Plants/PlantsPage.vue
+++ src/components/Plants/PlantsPage.vue
@@ -8,16 +8,13 @@
         <div class="keeper-button">
           <img src="@/assets/Logo/keeper.png" v-if="!show" @click="goToView(routeFollowing)" alt="Réaliser le suivi" />
         </div>
-
         <div class="follow-button">
           <img src="@/assets/Logo/following-plant.png" v-if="!show" @click="goToView(routeKeepering)"
             alt="Proposer un créneau de gardiennage" />
         </div>
-
         <div class="add-button">
           <img src="@/assets/Logo/add-button.png" @click="goToView(routeAddNewPlant)" alt="Ajouter une plante" />
         </div>
-
       </div>
     </div>
 
@@ -26,28 +23,26 @@
     </div>
 
     <div v-else class="plant-card">
-      <!-- In serer ici un v-for pour boucler sur les plantes et
-      les champs correspondant remplis par plant.value -->
       <PlantCard />
     </div>
   </div>
 </template>
 
 <script>
-import EmptyPlant from '@/components/Plants/EmptyPlant.vue'
-import PlantCard from '@/components/Plants/PlantCard.vue'
+import EmptyPlant from '@/components/Plants/EmptyPlant.vue';
+import PlantCard from '@/components/Plants/PlantCard.vue';
 
 export default {
   name: 'PlantsPage',
   components: {
     EmptyPlant,
-    PlantCard
+    PlantCard,
   },
   data: () => ({
     show: false,
     routeFollowing: 'addnewfollowing',
     routeKeepering: 'addnewkeepering',
-    routeAddNewPlant: 'addnewplants'
+    routeAddNewPlant: 'addnewplants',
 
   }),
   methods: {
@@ -85,25 +80,16 @@ export default {
   align-content: center;
   align-items: center;
   text-align: center;
-
 }
 
-.add-button>img {
-  width: 50px;
-  height: auto;
-  cursor: pointer;
-}
-
-.follow-button>img {
-  width: auto;
-  height: 50px;
-  cursor: pointer;
-}
-
+.add-button>img,
+.follow-button>img,
 .keeper-button>img {
   width: auto;
   height: 50px;
   cursor: pointer;
+  border: 1px solid var(--main-container-border);
+  box-shadow: 5px 5px 6px 1px var(--main-container-border);
 }
 
 .main-container {
@@ -131,8 +117,23 @@ export default {
 
 @media (max-width: 1000px) {
   .main-title {
-    flex: 1;
+    flex: 9;
     font-size: large;
+  }
+
+  .group-button-menu {
+    flex: 1;
+    gap: 2px;
+  }
+
+  .add-button>img,
+  .follow-button>img,
+  .keeper-button>img {
+    width: auto;
+    height: 35px;
+    cursor: pointer;
+    border: 1px solid var(--main-container-border);
+    box-shadow: 5px 5px 5px -1px var(--main-container-border);
   }
 }
 </style>
