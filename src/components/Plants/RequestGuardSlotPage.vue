d08955dd869e767c0bfcdcfa4d041e7ee39f2249
Thomas <thomas.le-corvec@outlook.com>
2023-03-14 19:17:41 +0100

    Add slot

--- src/components/Plants/RequestGuardSlotPage.vue
+++ src/components/Plants/RequestGuardSlotPage.vue
@@ -1,13 +1,56 @@
 <template>
   <div class="main-container">
     <h1>Page pour demander un creneau de gardiennage</h1>
-
+    <form @submit.prevent="createSlot">
+        <div class="form-field">
+            <label for="startDate">Date de début : </label>
+            <input
+              v-model="startDate"
+              type="date"
+              placeholder="Date de début"
+            />
+          </div>
+        <div class="form-field">
+            <label for="endDate">Date de fin : </label>
+            <input
+                v-model="endDate"
+                type="date"
+                placeholder="Date de fin"
+            />
+            </div>
+        <button class="btn-validate">Enregistrer</button>
+    </form>
   </div>
 </template>
 
 <script>
+import {config, getToken, getCurrentUserId} from '../../../api.config'
+
 export default {
-  name: 'RequestGuardSlotPage'
+  name: 'RequestGuardSlotPage',
+  data(){
+    return {
+        startDate: "",
+        endDate: ""
+    }
+  },
+  methods: {
+    createSlot(){
+        fetch(config.apiBase + config.endpoints.slotsPath, {
+            method: 'POST',
+            headers: {
+                Authorization: "Bearer " + getToken(),
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                startDate: this.startDate,
+                endDate: this.endDate,
+                userId: getCurrentUserId()
+            })
+        })
+    }
+  }
+
 }
 
 </script>
