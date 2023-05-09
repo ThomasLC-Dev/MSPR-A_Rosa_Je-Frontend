277c238e739411f26b55f20c21573ab839a8877f
Charlene <charlene.dhon@farsight-vr.com>
2023-05-09 22:04:34 +0200

    Placement marqueur et centrage map ok

--- src/components/Keeper/MapPage.vue
+++ src/components/Keeper/MapPage.vue
@@ -15,9 +15,9 @@ import { config, getToken, getCurrentUserId } from '../../../api.config'
 export default {
     name: 'MapPage',
     data() {
-   return{
-     center: [37,7749, -122,4194],
-	 user: {
+        return {
+            center: [37, 7749, -122, 4194],
+            user: {
                 "address": {
                     "additionalAddress": "string",
                     "city": "string",
@@ -46,95 +46,120 @@ export default {
                     }
                 ]
             },
-			latitude : 0, 
-			longitude : 0
-   }}, 
-   methods:{
-
-	GetUser() {
-      fetch("https://a-rosa-je.herokuapp.com/api/users/" + getCurrentUserId(), {
-        headers: {
-          Authorization: 'Bearer ' + getToken(),
+            plants: []
         }
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          this.user = data;
-		  console.log(this.user);
-		  this.SetValues();
-        })
+    },
+    methods: {
 
+        GetUser() {
+            return fetch("https://a-rosa-je.herokuapp.com/api/users/" + getCurrentUserId(), {
+                headers: {
+                    Authorization: 'Bearer ' + getToken(),
+                }
+            });
+
+        },
+        GetPlants() {
+            return fetch("https://a-rosa-je.herokuapp.com/api/slots/", {
+                headers: {
+                    Authorization: 'Bearer ' + getToken(),
+                }
+            });
+        }
 
     },
+    mounted() {
 
-	SetValues(){
-		this.latitude = this.user.address.latitude;
-		this.longitude = this.user.address.longitude;
+        this.GetUser().then((res) => res.json())
+            .then(
+                (userData) => {
 
-	}
-
-   },
-   beforeMount(){
-	this.GetUser();
-   },
-   mounted(){
-
-	console.log(this.latitude, this.longitude); 
-	const map = L.map('map').setView([48.117266, -1.6777926], 10);
-
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
-
-	const marker = L.marker([48.117266, -1.6777926]).addTo(map)
-		.bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
-
-	// const circle = L.circle([51.508, -0.11], {
-	// 	color: 'red',
-	// 	fillColor: '#f03',
-	// 	fillOpacity: 0.5,
-	// 	radius: 500
-	// }).addTo(map).bindPopup('I am a circle.');
-
-	// const polygon = L.polygon([
-	// 	[51.509, -0.08],
-	// 	[51.503, -0.06],
-	// 	[51.51, -0.047]
-	// ]).addTo(map).bindPopup('I am a polygon.');
+                    this.GetPlants().then((res) => res.json())
+                        .then(
+                            (plantData) => {
+                                this.plants = plantData,
+                                    console.log(this.plants);
+                                this.user = userData;
 
 
-	// const popup = L.popup()
-	// 	.setLatLng([51.513, -0.09])
-	// 	.setContent('I am a standalone popup.')
-	// 	.openOn(map);
+                                let latitude = this.user.address.latitude;
+                                let longitude = this.user.address.longitude;
 
-	function onMapClick(e) {
-		popup
-			.setLatLng(e.latlng)
-			.setContent(`You clicked the map at ${e.latlng.toString()}`)
-			.openOn(map);
-	}
+                                const map = L.map('map').setView([latitude, longitude], 10);
 
-	map.on('click', onMapClick);
-    
-   }
+                                const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                                    maxZoom: 19,
+                                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                                }).addTo(map);
+
+                                const markerUser = L.marker([latitude, longitude]).addTo(map)
+                                    .bindPopup("<b> C'est toi ! </b>").openPopup();
+
+                                for (let index = 0; index < this.plants.length; index++) {
+
+                                    if (this.plants[index].user.id != this.user.id) {
+
+                                        let strName = "Plante de : " + this.plants[index].user.firstName; 
+                                        console.log(this.plants[index].user);
+                                        const marker = L.marker([this.plants[index].user.address.latitude, this.plants[index].user.address.longitude]).addTo(map)
+                                            .bindPopup(strName);
+
+                                    }
+
+                                }
+
+
+                                // const circle = L.circle([51.508, -0.11], {
+                                // 	color: 'red',
+                                // 	fillColor: '#f03',
+                                // 	fillOpacity: 0.5,
+                                // 	radius: 500
+                                // }).addTo(map).bindPopup('I am a circle.');
+
+                                // const polygon = L.polygon([
+                                // 	[51.509, -0.08],
+                                // 	[51.503, -0.06],
+                                // 	[51.51, -0.047]
+                                // ]).addTo(map).bindPopup('I am a polygon.');
+
+
+                                // const popup = L.popup()
+                                // 	.setLatLng([51.513, -0.09])
+                                // 	.setContent('I am a standalone popup.')
+                                // 	.openOn(map);
+
+                                function onMapClick(e) {
+                                    popup
+                                        .setLatLng(e.latlng)
+                                        .setContent(`You clicked the map at ${e.latlng.toString()}`)
+                                        .openOn(map);
+                                }
+
+                                map.on('click', onMapClick);
+                            }
+                        )
+
+                }
+
+            )
+    }
 }
-  
-   
+
 
 
 </script>
 <style scoped>
-.main-container{
-	width: 100%;
+.main-container {
+    width: 100%;
 }
+
 #mapContainer {
- width: 80vw;
- height: 100vh;
+    width: 80vw;
+    height: 100vh;
 }
+
 .ButtonReturn {
-    left: 350px;
+    left: 250px;
     position: fixed;
 }
 
@@ -143,6 +168,6 @@ export default {
     border-radius: 20%;
     width: 100vh;
     height: 90vh;
-	margin: auto;
+    margin: auto;
 }
 </style>
